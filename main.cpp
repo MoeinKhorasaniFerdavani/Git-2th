7abd2dee6877eea73b0bbcecf796354b1a16dee9
MoeinKhorasaniFerdavani <65420194+MoeinKhorasaniFerdavani@users.noreply.github.com>
2020-05-19 18:45:56 +0430

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+// IN THE NAME OF GOD
+#include <iostream>
+using namespace std;
+
+class c
+{
+	int data;
+public:
+	c(int data = 0)
+	{
+		this->data = data;
+	}
+	void setData(int data)
+	{
+		this->data = data;
+	}
+	c operator= (int data)
+	{
+		this->data = data;
+	}
+
+};
+
+int main()
+{
+	cout << "Hello world" << endl;
+	c clas;
+	clas = 2;
+	system("pause");
+	return 0;
+}
