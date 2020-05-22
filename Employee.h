64eb077b3a9a268dba27ca20d96802c2d310b10e
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-05-22 23:04:40 +0430

    msd

--- Employee.h
+++ Employee.h
@@ -6,18 +6,13 @@ class Employee
 {
 	string family ,name;
 	int id, salery;
-<<<<<<< HEAD
+
 	string name;
 	string family;
 
-=======
+
 	
-<<<<<<< HEAD
-	//slkdfj
->>>>>>> new
-=======
 	
->>>>>>> new
 	
 public:
 	Employee();
