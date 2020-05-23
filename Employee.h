5b37c12dbd70f12f6dcb239149451d03dfe23bf2
MoeinKhorsaniFerdavani <moeinferdavani1380@gmail.com>
2020-05-23 11:16:49 +0430

    void employee

--- Employee.h
+++ Employee.h
@@ -1,17 +1,1 @@
-#pragma once
-#include <iostream>
-using namespace std;
-class Employee
-{
-	//hello
-	
-	//int id, salery;
-
-public:
-	Employee();
-	void setSalery(int);
-	Employee operator= (const Employee&);
-	~Employee();
-};
-
 //hello 
